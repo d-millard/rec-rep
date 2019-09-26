e8e92cbfecc903d29f8634336c9d976ea9f77d96
d-millard <54003433+d-millard@users.noreply.github.com>
2019-09-25 21:31:29 -0400

    Add files via upload

---
+++ ML_Learn/life_satisfaction_example_test.py
@@ -0,0 +1,18 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import sklearn.linear_model  # download with "pip install -U scikit-learn"
+
+"""
+finish later it is getting late and experiment with documentation
+get the data from:
+life satisfaction - https://homl.info/4
+gdp per capita - https://homl.info/5
+"""
+
+# load the data
+# oecd_bli = pd.read_csv()  # just getting files (maybe pandas works with them in a way), csv is comma separated values
+
+
+
