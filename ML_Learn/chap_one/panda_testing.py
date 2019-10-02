b23f3c8475b3a9909149dad4221b64972a36ac0f
d-millard <54003433+d-millard@users.noreply.github.com>
2019-10-01 21:18:01 -0400

    Add files via upload

---
+++ ML_Learn/chap_one/panda_testing.py
@@ -0,0 +1,8 @@
+# testing pandas out here
+"""
+either pandas documentation - "https://pandas.pydata.org/pandas-docs/stable/"
+or
+video - "https://www.youtube.com/results?search_query=pandas+python"
+"""
+
+
